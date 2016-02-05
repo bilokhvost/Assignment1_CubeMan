dacd561073697859407c77d2e632a541cc9e3b5c
Kateryna Bilokhvost <therry26@gmail.com>
2016-02-04 23:49:16 -0500

    coments were added

--- Scripts/objects/control.js
+++ Scripts/objects/control.js
@@ -1,4 +1,19 @@
-//Kateryna Bilokhvost
+/*controls.ts
+Kateryna Bilokhvost
+Last Modified by: Kateryna Bilokhvost
+Date last Modified: Feb 04, 2016
+This is a program for cube man display.
+The following controls are available: change rotation speed and change man's color
+Revision History:
+    Commit 1-3: Created the project file
+    Commit 4: Added the greem humonoid
+    Commit 5: Code optimization, adding new colors, fixed the camera
+    Commit 6: Rotation was added
+    Commit 7: Shadow issue is fixed
+    Commit 8: Possibility to change colors was added
+    Commit 9: Comments were added
+
+*/
 /// <reference path="../../typings/tsd.d.ts"/>
 var objects;
 (function (objects) {
@@ -11,6 +26,7 @@ var objects;
             this.rotationSpeedZ = rotationSpeed;
         }
         //PUBLIC METHODS +++++++++++++++++++++++++++++++++++++++
+        //random change of colors
         Control.prototype.randomColour = function () {
             legOne.material.setValues({ color: (Math.random() * 0xFFFFFF << 0) });
             legTwo.material.setValues({ color: (Math.random() * 0xFFFFFF << 0) });
