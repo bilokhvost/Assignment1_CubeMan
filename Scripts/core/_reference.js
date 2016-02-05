f1d0a37618f355b97b8f31f9849b08a1f8f96818
Kateryna Bilokhvost <therry26@gmail.com>
2016-02-05 12:45:08 -0500

    comment was added to _reference.ts

--- Scripts/core/_reference.js
+++ Scripts/core/_reference.js
@@ -1,3 +1,19 @@
+/*game.ts
+Kateryna Bilokhvost
+Last Modified by: Kateryna Bilokhvost
+Date last Modified: Feb 04, 2016
+This is a program for cube man display.
+The following controls are available: change rotation speed and change man's color
+Revision History:
+    Commit 1-3: Created the project file
+    Commit 4: Added the greem humonoid
+    Commit 5: Code optimization, adding new colors, fixed the camera
+    Commit 6: Rotation was added
+    Commit 7: Shadow issue is fixed
+    Commit 8: Possibility to change colors was added
+    Commit 9: Comments were added
+
+*/
 /// <reference path="../../typings/tsd.d.ts" />
 /// <reference path="../objects/point.ts"/>
 /// <reference path="../objects/control.ts" />
